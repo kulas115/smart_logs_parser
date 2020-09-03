ecab651c9df72890a2c50ea5fef1b804daed06c7
kulas115 <kulas115@gmail.com>
2020-09-03 07:34:13 +0200

    add missing magic comment

--- lib/logs_parser/counter.rb
+++ lib/logs_parser/counter.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 module LogsParser
-class Counter
-  def call(visits, ordering = nil)
-    visits.each_with_object({}) do |(page_url, ips_array), views|
-      views[page_url] = if ordering == :uniq
-                          ips_array.uniq.size
-                        else
-                          ips_array.size
-                        end
+  class Counter
+    def call(visits, ordering = nil)
+      visits.each_with_object({}) do |(page_url, ips_array), views|
+        views[page_url] = if ordering == :uniq
+                            ips_array.uniq.size
+                          else
+                            ips_array.size
+                          end
+      end
     end
   end
-end
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'logs_parser'
 
 class Parser
