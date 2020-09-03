e56826e6c7fae257d825c63f17394ffb34c63530
kulas115 <kulas115@gmail.com>
2020-09-03 07:34:22 +0200

    create tempfile inside spec

--- spec/integration/parser_spec.rb
+++ spec/integration/parser_spec.rb
@@ -1,7 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe Parser do
-  let(:sample_log_path) { './sample_webserver.log' }
+  let(:log_file)        { Tempfile.new(SecureRandom.alphanumeric) }
+  let(:sample_log_path) { log_file.path }
+  let(:logs) do
+    <<~LOGS
+      /help_page/1 126.318.035.038
+      /help_page/1 126.318.035.038
+      /help_page/1 184.123.665.067
+      /help_page/1 126.318.035.038
+      /help_page/1 444.701.448.104
+      /help_page/1 929.398.951.889
+      /contact 184.123.665.067
+      /contact 126.318.035.038
+      /contact 444.701.448.104
+      /contact 184.123.665.067
+      /home 184.123.665.067
+      /home 444.701.448.104
+      /home 184.123.665.067
+      /about/2 444.701.448.104
+      /about/2 444.701.448.104
+      /index 444.701.448.104
+    LOGS
+  end
+
+  before do
+    log_file.write(logs)
+    log_file.rewind
+  end
+
+  after { log_file.close }
 
   describe '#call' do
     subject(:parse) { described_class.new(file_path: sample_log_path, schema: schema).call }
@@ -13,10 +41,9 @@ RSpec.describe Parser do
         <<~TEXT
           /help_page/1 6 visits
           /contact 4 visits
-          /home 4 visits
-          /about/2 4 visits
-          /index 4 visits
-          /about 3 visits
+          /home 3 visits
+          /about/2 2 visits
+          /index 1 visits
         TEXT
       end
 
@@ -30,12 +57,11 @@ RSpec.describe Parser do
 
       let(:results) do
         <<~TEXT
-          /help_page/1 5 unique views
-          /home 4 unique views
-          /index 4 unique views
+          /help_page/1 4 unique views
           /contact 3 unique views
-          /about/2 3 unique views
-          /about 3 unique views
+          /home 2 unique views
+          /about/2 1 unique views
+          /index 1 unique views
         TEXT
       end
 
@@ -51,10 +77,9 @@ RSpec.describe Parser do
         <<~TEXT
           /help_page/1 6 visits
           /contact 4 visits
-          /home 4 visits
-          /about/2 4 visits
-          /index 4 visits
-          /about 3 visits
+          /home 3 visits
+          /about/2 2 visits
+          /index 1 visits
         TEXT
       end
 
