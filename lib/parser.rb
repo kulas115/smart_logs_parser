b5a1213de2e89988609ef4a371a54011c9aad332
kulas115 <kulas115@gmail.com>
2020-09-03 07:34:19 +0200

    predefine schemes and test possible call scenarios

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,14 +5,30 @@ require_relative 'environment'
 class Parser
   SCHEMES = {
     total: {
-
+      counter: LogsParser::Counter::Total,
+      printer: LogsParser::Printer::Total
     },
     unique: {
-
+      counter: LogsParser::Counter::Unique,
+      printer: LogsParser::Printer::Unique
     }
   }.freeze
 
-  def call(file_path)
-    LogsParser::Worker.new.call(file_path)
+  def initialize(file_path:, schema: nil)
+    @file_path = file_path
+    @schema = schema
   end
+
+  def call
+    selected_schema = SCHEMES[schema]
+
+    LogsParser::Worker.new(input_path: file_path,
+                           counter: selected_schema&.dig(:counter),
+                           printer: selected_schema&.dig(:printer))
+                      .call
+  end
+
+  private
+
+  attr_reader :file_path, :schema
 end
---
+++ spec/integration/parser_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+RSpec.describe Parser do
+  let(:sample_log_path) { './sample_webserver.log' }
+
+  describe '#call' do
+    subject(:parse) { described_class.new(file_path: sample_log_path, schema: schema).call }
+
+    context 'when using :total schema' do
+      let(:schema) { :total }
+
+      let(:results) do
+        <<~TEXT
+          /help_page/1 6 visits
+          /contact 4 visits
+          /home 4 visits
+          /about/2 4 visits
+          /index 4 visits
+          /about 3 visits
+        TEXT
+      end
+
+      it 'it prints total visits to the srdout' do
+        expect { parse }.to output(results).to_stdout
+      end
+    end
+
+    context 'when using :unique schema' do
+      let(:schema) { :unique }
+
+      let(:results) do
+        <<~TEXT
+          /help_page/1 5 unique views
+          /home 4 unique views
+          /index 4 unique views
+          /contact 3 unique views
+          /about/2 3 unique views
+          /about 3 unique views
+        TEXT
+      end
+
+      it 'it prints unique visits to the srdout' do
+        expect { parse }.to output(results).to_stdout
+      end
+    end
+
+    context 'when schema is not specified' do
+      let(:schema) { :nil }
+
+      let(:results) do
+        <<~TEXT
+          /help_page/1 6 visits
+          /contact 4 visits
+          /home 4 visits
+          /about/2 4 visits
+          /index 4 visits
+          /about 3 visits
+        TEXT
+      end
+
+      it 'it prints total visits to the srdout' do
+        expect { parse }.to output(results).to_stdout
+      end
+    end
+  end
+end
