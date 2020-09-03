39fa2069b8c716e56971543130ec6bb24e28cda7
kulas115 <kulas115@gmail.com>
2020-09-03 07:34:17 +0200

    move dependencies requires to parser class

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
-require_relative 'logs_parser'
+require 'pry'
+require_relative 'logs_parser/reader'
+require_relative 'logs_parser/line_splitter'
+require_relative 'logs_parser/storer'
+require_relative 'logs_parser/counter/base'
+require_relative 'logs_parser/counter/total'
+require_relative 'logs_parser/counter/unique'
+require_relative 'logs_parser/sorter'
+require_relative 'logs_parser/printer/base'
+require_relative 'logs_parser/printer/total'
+require_relative 'logs_parser/printer/unique'
 
 class Parser
   SCHEMES = {
@@ -10,8 +20,8 @@ class Parser
     unique: {
 
     }
-  }
-  
+  }.freeze
+
   def call(file_path)
     LogsParser::Worker.new.call(file_path)
   end
