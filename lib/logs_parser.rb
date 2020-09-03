be69549cc5118af8ac1587df6f649e1ebc0b52b8
kulas115 <kulas115@gmail.com>
2020-09-03 07:34:16 +0200

    use polymprphic classess to get total and unique visits

--- lib/logs_parser.rb
+++ lib/logs_parser.rb
@@ -4,9 +4,13 @@ require 'pry'
 require_relative 'logs_parser/reader'
 require_relative 'logs_parser/line_splitter'
 require_relative 'logs_parser/storer'
-require_relative 'logs_parser/counter'
+require_relative 'logs_parser/counter/base'
+require_relative 'logs_parser/counter/total'
+require_relative 'logs_parser/counter/unique'
 require_relative 'logs_parser/sorter'
-require_relative 'logs_parser/printer'
+require_relative 'logs_parser/printer/base'
+require_relative 'logs_parser/printer/total'
+require_relative 'logs_parser/printer/unique'
 
 module LogsParser
   class Worker
--- spec/integration/logs_parser_spec.rb
+++ spec/integration/logs_parser_spec.rb
@@ -2,20 +2,53 @@
 
 require 'spec_helper'
 RSpec.describe LogsParser do
-  it 'accepts the file_path and prints total vists per IP sorted desc' do
-    sample_log_path = './sample_webserver.log'
+  let(:sample_log_path) { './sample_webserver.log' }
 
-    worker = LogsParser::Worker.new(input_path: sample_log_path)
+  describe '#call' do
+    context 'when passing Total classes' do
+      subject(:parse) do
+        LogsParser::Worker.new(input_path: sample_log_path,
+                               counter: LogsParser::Counter::Total.new,
+                               printer: LogsParser::Printer::Total.new).call
+      end
 
-    result = <<~TEXT
-      /help_page/1 6 visits
-      /contact 4 visits
-      /home 4 visits
-      /about/2 4 visits
-      /index 4 visits
-      /about 3 visits
-    TEXT
+      let(:results) do
+        <<~TEXT
+          /help_page/1 6 visits
+          /contact 4 visits
+          /home 4 visits
+          /about/2 4 visits
+          /index 4 visits
+          /about 3 visits
+        TEXT
+      end
 
-    expect { worker.call }.to output(result).to_stdout
+      it 'it prints total visits to the srdout' do
+        expect { parse }.to output(results).to_stdout
+      end
+    end
+
+    context 'when passing Unique classes' do
+      subject(:parse) do
+        LogsParser::Worker.new(input_path: sample_log_path,
+                               counter: LogsParser::Counter::Unique.new,
+                               printer: LogsParser::Printer::Unique.new).call
+      end
+
+      let(:results) do
+        <<~TEXT
+          /help_page/1 5 unique views
+          /home 4 unique views
+          /index 4 unique views
+          /contact 3 unique views
+          /about/2 3 unique views
+          /about 3 unique views
+        TEXT
+      end
+
+      it 'it prints total visits to the srdout' do
+        expect { parse }.to output(results).to_stdout
+      end
+    end
   end
 end
